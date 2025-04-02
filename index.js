1952c54940bda8e9d774f3d18b789a026a9b7e86
amarmerada <67133103+amarmerada@users.noreply.github.com>
2025-04-02 13:30:50 -0400

    refactored index file

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
-const http = require('http');
-const fs = require('fs');
-const path = require('path');
 const express = require('express');
+const path = require('path');
 const app = express();
 
-const server = http.createServer((req, res) => {
-    
+// Serve static files from the current directory
+app.use(express.static(path.join(__dirname)));
 
-    const filePath = path.join(__dirname, 'index.html');
-
-    fs.readFile(filePath, (err, content) => {
-        if (err) {
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('Error loading the HTML file');
-            console.log(err);
-        } else {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
-            res.end(content);
-        }
-  });
+// Route for the main page
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.use(express.static('/'));
-
 const port = 3000;
-
-server.listen(port, () => {
+app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
 });
